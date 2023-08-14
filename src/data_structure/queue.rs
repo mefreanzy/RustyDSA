798ed3d66ed48a15cce4eba9e3dfad0df5239cbd
mefreanzy <freanzyme@gmail.com>
2023-08-14 04:45:52 +0300

    commit

--- src/data_structure/queue.rs
+++ src/data_structure/queue.rs
@@ -1,3 +1,10 @@
+//! # Queue Data Structure
+//!
+//! A queue is a linear data structure that follows the First-In-First-Out (FIFO) principle. Elements are added at the rear, or "enqueue" operation, and removed from 
+//! the front, or "dequeue" operation, maintaining their original order. This structure simulates a real-life queue, where the first person to join is the first to be 
+//! served. Queues are used in various applications such as task scheduling, breadth-first searches, and print spooling. Basic operations on a queue include enqueue 
+//! (adding an element), dequeue (removing the front element), and peek (viewing the front element without removal).
+
 use std::collections::LinkedList;
 
 #[derive(Debug)]
@@ -8,6 +15,13 @@ pub struct Queue<T>
 
 impl<T> Queue<T>
 {
+    /// Creates a new empty queue.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let queue: Queue<i32> = Queue::new();
+    /// ```
     pub fn new() -> Self
     {
         Queue {
@@ -15,21 +29,60 @@ impl<T> Queue<T>
         }
     }
 
+    /// Adds an element to the end of the queue.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut queue: Queue<i32> = Queue::new();
+    /// queue.enqueue(1);
+    /// assert_eq!(queue.peek(), Some(&1));
+    /// ```
     pub fn enqueue(&mut self, element: T)
     {
         self.data.push_back(element)
     }
 
+     /// Removes and returns an element from the front of the queue.
+    /// Returns `None` if the queue is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut queue: Queue<i32> = Queue::new();
+    /// queue.enqueue(1);
+    /// let element = queue.dequeue();
+    /// assert_eq!(element, Some(1));
+    /// ```
     pub fn dequeue(&mut self) -> Option<T>
     {
         self.data.pop_front()
     }
 
+    /// Checks if the queue is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut queue: Queue<i32> = Queue::new();
+    /// queue.enqueue(1);
+    /// assert!(!queue.is_null());
+    /// ```
     pub fn is_null(&mut self) -> bool
     {
         self.data.is_empty()
     }
 
+    /// Peeks at the front element of the queue without removing it.
+    /// Returns `None` if the queue is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut queue: Queue<i32> = Queue::new();
+    /// queue.enqueue(1);
+    /// assert_eq!(queue.peek(), Some(&1));
+    /// ```
     pub fn peek(&mut self) -> Option<&T>
     {
         self.data.front()
--- src/data_structure/stack.rs
+++ src/data_structure/stack.rs
@@ -1,3 +1,10 @@
+//! # Stack Data Structure
+//!
+//! A stack is a linear data structure that follows the Last-In-First-Out (LIFO) principle. 
+//! Elements are added and removed from the top, or "topmost" end of the stack. It operates like a collection of items stacked on top of each other, where the last item added is the first to be removed. 
+//! Stacks are used in various applications, such as managing function calls in programming, tracking navigation history, and implementing undo operations. 
+//! Common operations on a stack include push (adding an element), pop (removing the top element), and peek (viewing the top element without removal).
+
 #[derive(Debug)]
 pub struct Stack<T>
 {
@@ -6,6 +13,13 @@ pub struct Stack<T>
 
 impl<T> Stack<T>
 {
+    /// Creates a new empty stack.
+    /// 
+    /// # Examples
+    ///
+    /// ```
+    /// let stack: Stack<i32> = Stack::new();
+    /// ```
     pub fn new() -> Self
     {
         Stack {
@@ -13,21 +27,60 @@ impl<T> Stack<T>
         }
     }
 
+    /// Adds elements to the stack.
+    /// 
+    /// # Examples
+    ///
+    /// ```
+    /// let mut stack: Stack<i32> = Stack::new();
+    /// stack.push(1);
+    /// assert!(!is_null());
+    /// ```
     pub fn push(&mut self, element: T)
     {
         self.data.push(element)
     }
 
+    /// Removes and returns an element from the stack.
+    /// Returns `None` if the stack is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut stack: Stack<i32> = Stack::new();
+    /// stack.push(1);
+    /// let element = stack.pop();
+    /// assert_eq!(element, Some(1));
+    /// ```
     pub fn pop(&mut self) -> Option<T>
     {
         self.data.pop()
     }
 
+    /// Checks if the stack is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut stack: Stack<i32> = Stack::new();
+    /// stack.push(1);
+    /// assert!(!stack.is_null());
+    /// ```
     pub fn is_null(&mut self) -> bool
     {
         self.data.is_empty()
     }
 
+    /// Peeks at the top element of the stack without removing it.
+    /// Returns `None` if the stack is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let mut stack: Stack<i32> = Stack::new();
+    /// stack.push(1);
+    /// assert_eq!(stack.peek(), Some(&1));
+    /// ```
     pub fn peek(&mut self) -> Option<&T>
     {
         self.data.last()
